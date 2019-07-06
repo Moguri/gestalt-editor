eb1a1488288f77f4544094d31034619fb46d9a9f
Mitchell Stokes <mogurijin@gmail.com>
2019-07-06 16:41:14 -0700

    viewport: Load a model if one is given as an argument

--- gestalt/components.py
+++ gestalt/components.py
@@ -1,6 +1,9 @@
+import sys
+
 import panda3d.core as p3d
 from direct.showbase.DirectObject import DirectObject
 
+import gestalt.assetlibrary
 import gestalt.floorplane
 
 
@@ -19,13 +22,29 @@ class ViewportComponent(EditorComponent):
     def __init__(self):
         super().__init__()
 
-        self.floorplane = gestalt.floorplane.FloorPlane(scale=1_000)
+        self.assetlib = gestalt.assetlibrary.AssetLibrary()
         self.camera = None
+
+        # Viewport Widgets
         self.viewport_widgets = p3d.NodePath('Viewport Widgets')
+        self.floorplane = gestalt.floorplane.FloorPlane(scale=1_000)
+        self.floorplane.nodepath.reparent_to(self.viewport_widgets)
+
+        # Scene
+        self.scene_root = p3d.NodePath('Scene Root')
 
     def setup(self, showbase):
+        self.viewport_widgets.reparent_to(showbase.render)
+        self.scene_root.reparent_to(showbase.render)
+
+        # Add a better starting camera position
         self.camera = showbase.cam
         self.camera.set_pos(10, -10, 10)
         self.camera.look_at(0, 0, 0)
-        self.floorplane.nodepath.reparent_to(self.viewport_widgets)
-        self.viewport_widgets.reparent_to(showbase.render)
+
+        # Try to load a model
+        if len(sys.argv) > 1:
+            modelpath = sys.argv[1]
+            self.assetlib.import_file(modelpath)
+            model_root = self.assetlib.libraries[0]
+            self.scene_root.attach_new_node(model_root)
